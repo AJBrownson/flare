0b93fc3c65c2687535dc3a8ec92c0721a992feb4
Anietie Brownson <92381518+AJBrownson@users.noreply.github.com>
2024-06-06 11:28:59 +0100

    fix: Fix style changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,30 +1,9 @@
-"use client";
-import { useState } from "react";
-import Image from "next/image"
-import InsufficientFundsModal from "@/components/basic-wheel/insufficientFundsModal"
-import WalletConnectionModal from "@/components/basic-wheel/walletConnectModal";
-import ClaimPageModal from "@/components/basic-wheel/claimPageModal";
-import PrizeModal from "@/components/basic-wheel/prizes";
-
+import Users from "@/components/userComponent/users"
 
 export default function Testing() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <>
-      <button onClick={openModal}>CLICK ME</button>
-      <ClaimPageModal isOpen={isModalOpen} onClose={closeModal} />
-      {/* <PrizeModal isOpen={isModalOpen} onClose={closeModal} /> */}
-      {/* <InsufficientFundsModal isOpen={isModalOpen} onClose={closeModal} /> */}
-     {/* <WalletConnectionModal isOpen={isModalOpen} onClose={closeModal} /> */}
+    <Users />
     </>
-  );
+  )
 }
--- components/userComponent/users.tsx
+++ components/userComponent/users.tsx
@@ -30,17 +30,17 @@ export default function Users() {
         <section className="py-6 flex justify-center items-center">
           <div className="bg-[#191815] w-full flex flex-col justify-center items-center max-w-[325px] border border-[#30302B] rounded-lg">
             <Image src={ProfilePic} alt="user profile picture" className="py-6" />
-            <div className="p-4 flex justify-between items-center border border-[#30302B] w-full">
+            <div className="p-4 flex justify-between items-center border-t border-[#30302B] w-full">
               <span className="flex justify-between items-center gap-1">
                 <Image src={Medal} alt="" />
                 <p>Player&apos;s Rank</p>
               </span>
-              <p className="border border-[#30302B] rounded-full text-center w-24 text-sm py-1 text-transparent font-bold bg-clip-text bg-gradient-to-b from-[#FFFE89] from-10% to-[#C65E34] to-100%">
+              <p className="rounded-full text-center w-24 text-sm py-1 text-transparent font-bold bg-clip-text bg-gradient-to-b from-[#FFFE89] from-10% to-[#C65E34] to-100%">
                 {rank}
                 {rankSuffix(parseInt(rank))}
               </p>
             </div>
-            <div className="p-4 flex justify-between items-center border border-[#30302B] w-full">
+            <div className="p-4 flex justify-between items-center border-t border-[#30302B] w-full">
               <span className="flex justify-between items-center gap-1">
                 <Image src={Medal} alt="" />
                 <p>{walletAddress}</p>
@@ -54,7 +54,7 @@ export default function Users() {
 
         <section>
           <div className="mb-4 py-6 bg-[#191815] border border-[#30302B] w-full rounded-lg px-4">
-            <p className="mb-6 border border-[#30302B] rounded-lg text-center w-36 text-sm py-2 font-bold">
+            <p className="mb-6 border border-[#30302B] rounded-lg text-center w-36 text-sm py-2 font-medium">
               Airdrop Allocation
             </p>
             <h1 className="font-extrabold text-xl mb-3 font-montserrat">
@@ -65,15 +65,15 @@ export default function Users() {
               miss out—take action to meet the requirements and secure your spot
               for the airdrop!
             </p>
-            <button className="rounded-lg bg-[#30302B] hover:bg-[#000] py-2 w-28 border border-[#30302B]">
+            <button className="text-sm rounded-lg bg-[#30302B] hover:bg-[#000] py-3 w-28 border border-[#30302B]">
               Check How
             </button>
           </div>
 
           <div className="py-6 bg-[#191815] border border-[#30302B] w-full rounded-lg px-4">
             <span className="bg-[#DC1FFF] flex items-center justify-center gap-2 w-28 mb-6 border border-[#30302B] rounded-lg text-center p-2 text-sm font-bold">
-              <Image src={Timer} alt="" className="w-6 h-6" />
-              <p>30 days</p>
+              <Image src={Timer} alt="" className="w-5 h-5" />
+              <p>40 days</p>
             </span>
             <h1 className="font-extrabold text-xl mb-3 font-montserrat">
               Join us and grab a share of 10,000,000 $SOLGACY!
@@ -82,7 +82,7 @@ export default function Users() {
               Climb to the top of the leaderboard to become a top player and
               claim your share of SOLGACY!
             </p>
-            <button className="rounded-lg bg-[#30302B] hover:bg-[#000] py-2 w-28 border border-[#30302B]">
+            <button className="text-sm rounded-lg bg-[#30302B] hover:bg-[#000] py-3 w-28 border border-[#30302B]">
               Spin & Win
             </button>
           </div>

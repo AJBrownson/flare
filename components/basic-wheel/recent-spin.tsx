e350a913e19e252eedc8e33bd2a97c916b988124
Anietie Brownson <92381518+AJBrownson@users.noreply.github.com>
2024-06-07 22:53:54 +0100

    fix: Add minor changes

--- components/basic-wheel/recent-spin.tsx
+++ components/basic-wheel/recent-spin.tsx
@@ -23,47 +23,53 @@ export default function RecentSpin() {
     </div>
   );
 
+  if (!data || !data.data) {
+    return <div>{skeletonLoader}</div>;
+  }
+
   return (
     <>
-      {isLoading && !data?.data ? (
-        <>
-          {skeletonLoader}
-          {skeletonLoader}
-          {skeletonLoader}
-          {skeletonLoader}
-          {skeletonLoader}
-          {skeletonLoader}
-        </>
-      ) : (
-        data.data.map((spin: Spin, i: number) => (
-          <div
-            key={i}
-            className={`px-5 pb-1 ${
-              i % 2 === 0 ? "bg-[#10100E]" : "bg-[#191815]"
-            }`}
-          >
-            <div className="flex justify-between items-center w-full py-2 rounded-md">
-              <p className="text-xs text-white w-1/3">
-                {spin.address.slice(0, 5)}...{spin.address.slice(-4)}
-              </p>
-
-              <div className="px-5 border border-transparent border-x-[#30302B] flex items-center gap-1">
-                <p
-                  className={`text-xs font-semibold text-center rounded-3xl ${
-                    spin.outcome === OUTCOME.WIN
-                      ? "bg-[#8DD6AF] text-[#1B874D] py-[2px] px-3 w-16"
-                      : "bg-[#FFD6C5] text-[#FF4E00] py-[2px] px-3 w-16"
-                  }`}
-                >
-                  {spin.outcome}
+      <div className="w-[320px] h-[470px] xl:max-w-[380px] overflow-y-auto">
+        {isLoading && !data?.data ? (
+          <>
+            {skeletonLoader}
+            {skeletonLoader}
+            {skeletonLoader}
+            {skeletonLoader}
+            {skeletonLoader}
+            {skeletonLoader}
+          </>
+        ) : (
+          data.data.map((spin: Spin, i: number) => (
+            <div
+              key={i}
+              className={`px-5 pb-1 ${
+                i % 2 === 0 ? "bg-[#10100E]" : "bg-[#191815]"
+              }`}
+            >
+              <div className="flex justify-between items-center w-full py-2 rounded-md">
+                <p className="text-xs text-white w-1/3">
+                  {spin.address.slice(0, 5)}...{spin.address.slice(-4)}
                 </p>
-              </div>
 
-              <p className="ml-4 text-xs text-white">{spin.name}</p>
+                <div className="px-5 border border-transparent border-x-[#30302B] flex items-center gap-1">
+                  <p
+                    className={`text-xs font-semibold text-center rounded-3xl ${
+                      spin.outcome === OUTCOME.WIN
+                        ? "bg-[#8DD6AF] text-[#1B874D] py-[2px] px-3 w-16"
+                        : "bg-[#FFD6C5] text-[#FF4E00] py-[2px] px-3 w-16"
+                    }`}
+                  >
+                    {spin.outcome}
+                  </p>
+                </div>
+
+                <p className="ml-4 text-xs text-white">{spin.name}</p>
+              </div>
             </div>
-          </div>
-        ))
-      )}
+          ))
+        )}
+      </div>
     </>
   );
 }
